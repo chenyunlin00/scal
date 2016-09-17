cc0d22d5adbd5f3b9f85135a078884f1365fc01e
chenyunlin00 <u_u_u@126.com>
2016-09-17 06:16:46 -0700

    add spider

---
+++ spider.py
@@ -0,0 +1,38 @@
+import urllib2
+import urllib
+import time
+from bs4 import BeautifulSoup
+#http://ffp.sichuanair.com/FFPNewWeb/Mall/Detail/8761
+#response = urllib2.urlopen("http://ffp.sichuanair.com/FFPNewWeb/Mall/Detail/6562")
+#response = urllib2.urlopen("http://ffp.sichuanair.com/FFPNewWeb/Mall/Detail/8761")
+#response = urllib2.urlopen("http://ffp.sichuanair.com/FFPNewWeb/Mall")
+#print response.read()
+#html = response.read()
+baseurl='http://ffp.sichuanair.com'
+getlist_url='http://ffp.sichuanair.com/FFPNewWeb/Mall/GetList'
+
+html = open('index.html').read()
+#print html
+soup = BeautifulSoup(html, "html.parser")
+#print soup.prettify()
+for a in soup.select('.dl_Category'):
+    for b in a.find_all('a'):
+	url=baseurl+b['href']
+	if url != 'http://ffp.sichuanair.com/FFPNewWeb/Mall/List/SM':
+		continue
+	post_para={'ID':'SM', 'OrderType':'MIL', 'PageIndex':1, 'PageSize':0}
+	post_data=urllib.urlencode(post_para)
+	print url
+	req=urllib2.Request(getlist_url, post_data)
+	req.add_header('Content-Type', "application/x-www-form-urlencoded")
+	response = urllib2.urlopen(req)
+	cate_html=response.read()
+	#print cate_html
+	cate_soup=BeautifulSoup(cate_html, "html.parser")   #category page
+	print cate_soup
+	for item in cate_soup.select('.ProductList1'):
+		print item
+		break
+	time.sleep(1)
+	#print "href=", url, "text=", b.get_text()
+        #print b['href']
